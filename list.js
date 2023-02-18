86f00827bb4d46d7d8347d5b683fe8ce1a481ab5
YUxiangLuo <871884617@qq.com>
2023-02-18 19:36:35 +0800

    1

--- list.js
+++ list.js
@@ -1,4 +1,5 @@
 module.exports = [
-    "https://tidal.com/browse/album/144918082",
-    "https://tidal.com/browse/album/144888817"
+    "https://tidal.com/browse/album/40546281",
+    "https://tidal.com/browse/album/96383304",
+    "https://tidal.com/browse/album/3267679"
 ]
